e880fca0bbea2e40c4d45684f48e84c392f2db92
Priya <priya.maheshwari_cs19@gla.ac.in>
2023-05-08 21:19:29 +0530

    commit

---
+++ src/com/company/oops/Assignment2Q7.java
@@ -0,0 +1,41 @@
+package com.company.oops;
+
+abstract class DesertItem {
+
+    abstract public int getCost();
+}
+class Candy extends DesertItem {
+    public int addCandies(int candies){
+
+    }
+
+    @Override
+    public int getCost() {
+        return 0;
+    }
+}
+class Cookie extends DesertItem {
+    public int addCookies(int candies){}
+
+    @Override
+    public int getCost() {
+        return 0;
+    }
+}
+class IceCream extends DesertItem {
+    public int addIceCreams(int candies){}
+
+    @Override
+    public int getCost() {
+        return 0;
+    }
+}
+public class Assignment2Q7 {
+    public static void main(String[] args) {}
+    private void selectRoles(){}
+    private void roles(String role){}
+    private void addItems() {}
+    private void addItemsOperation(int choice) {}
+    private void placeOrder() {}
+    private void placeOrderOperation(int choice) {}
+}
