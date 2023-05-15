82b82f7dea640535f063f9902fb90018e15d8f79
Priya <priya.maheshwari_cs19@gla.ac.in>
2023-05-15 21:37:33 +0530

    commit

---
+++ src/com/company/basic_DS/Assignment1Q6.java
@@ -0,0 +1,36 @@
+package com.company.basic_DS;
+
+import java.util.Scanner;
+
+class Login1{
+    String userId = "Ajay",password = "password";
+    public String loginUser(String user, String pass) {
+        if(user.equals(userId) && pass.equals(password)){
+            return "Welcome "+user;
+        }
+        else if (user.equals(userId) && pass.equals(password)){
+            return "You have entered wrong credentials ,please enter the right credentials.";
+        }
+        else{
+            return "You have entered wrong credentials 3 times";
+        }
+    }
+
+}
+public class Assignment1Q6
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Login1 object = new Login1();
+        int count =3;
+        while(count!=3){
+            System.out.println("Enter UserId");
+            String userId = sc.next();
+            System.out.println("Enter Password");
+            String password = sc.next();
+            System.out.println(object.loginUser(userId,password));
+        }
+
+
+    }
+}
