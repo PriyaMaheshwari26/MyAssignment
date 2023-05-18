e3f09b12e57edfc7cd053c787b3dc8759eadb9ac
Priya <priya.maheshwari_cs19@gla.ac.in>
2023-05-18 22:00:06 +0530

    commit

--- src/com/company/collections/Assignment3Q2.java
+++ src/com/company/collections/Assignment3Q2.java
@@ -1,7 +1,16 @@
 package com.company.collections;
 
+import java.util.HashSet;
+
 public class Assignment3Q2 {
+    class  Product{
+
+    }
     public static void main(String[] args) {
+        /*Write an application to store 10 unique product objects. In case there is an attempt to add a
+        duplicate product, it should be silently rejected. Hint: Use HasSet or TreeSet
+        Extra(optional): Use ArrayList in the above solution. (This is optional)*/
+        HashSet<Product> hashSet=new HashSet<>();
 
     }
 }
